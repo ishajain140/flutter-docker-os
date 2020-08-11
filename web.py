b7c8845f60265f4a27d263f69890dc60cc22b26c
ishajain140 <64470818+ishajain140@users.noreply.github.com>
2020-08-11 13:30:14 +0530

    python file
    create this in /var/www/cgi-bin/ folder of your base system

---
+++ web.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+import cgi
+import subprocess
+print("content-type: text/html")
+print()
+mydata=cgi.FieldStorage()
+osname=mydata.getvalue("x")
+imagename=mydata.getvalue("y")
+cmd ="sudo docker run -dit --name {} {}".format(osname, imagename)
+output = subprocess.getoutput(cmd)
+# output=subprocess.getoutput(name)
+print(output)
